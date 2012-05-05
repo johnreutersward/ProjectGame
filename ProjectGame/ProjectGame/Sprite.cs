4a7a0ae2c867104250b1c8b51d437e3be69c03fd
rojters <rojters@gmail.com>
2012-05-05 10:16:20 +0200

    first version of abstract Sprite class

---
+++ ProjectGame/ProjectGame/Sprite.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjectGame
+{
+    abstract class Sprite
+    {
+        Texture2D textureImage;
+        protected Point frameSize;
+        Point currentFrame;
+        Point sheetSize;
+        int collisionOffset;
+        int timeSinceLastFrame = 0;
+        int millisecondsPerFrame;
+        const int defaultMillisecondsPerFrame = 16;
+        protected Vector2 speed;
+        protected Vector2 position;
+
+        public Sprite(Texture2D textureImage, Vector2 position, Point frameSize, int collisionOffset, Point currentFrame, Point sheetSize, Vector2 speed) 
+            : this(textureImage, position, frameSize, collisionOffset, currentFrame, sheetSize, speed, defaultMillisecondsPerFrame)
+        {}
+
+        public Sprite(Texture2D textureImage, Vector2 position, Point frameSize, int collisionOffset, Point currentFrame, Point sheetSize, Vector2 speed, int millisecondsPerFrame)
+        {
+            this.textureImage = textureImage;
+            this.position = position;
+            this.frameSize = frameSize;
+            this.collisionOffset = collisionOffset;
+            this.currentFrame = currentFrame;
+            this.sheetSize = sheetSize;
+            this.speed = speed;
+            this.millisecondsPerFrame = millisecondsPerFrame;
+        }
+
+    }
+}
